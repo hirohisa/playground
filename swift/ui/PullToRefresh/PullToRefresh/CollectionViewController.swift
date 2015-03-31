37a155aa20074f924030cb4dd97e2d6371047db8
Hirohisa Kawasaki <hirohisa.kawasaki@gmail.com>
2015-04-01 01:05:06 +0900

    Create PullToRefresh project

---
+++ swift/ui/PullToRefresh/PullToRefresh/CollectionViewController.swift
@@ -0,0 +1,13 @@
+//
+//  CollectionViewController.swift
+//  PullToRefresh
+//
+//  Created by Hirohisa Kawasaki on 4/1/15.
+//  Copyright (c) 2015 Hirohisa Kawasaki. All rights reserved.
+//
+
+import UIKit
+
+public class CollectionViewController: UICollectionViewController {
+
+}
