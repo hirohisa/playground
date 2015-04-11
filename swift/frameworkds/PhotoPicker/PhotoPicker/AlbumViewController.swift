7c88730a932161d38303e011b3faf431fbd8ad2c
Hirohisa Kawasaki <hirohisa.kawasaki@gmail.com>
2015-04-12 04:29:00 +0900

    Add `PHCollectionList`

--- swift/frameworkds/PhotoPicker/PhotoPicker/AlbumViewController.swift
+++ swift/frameworkds/PhotoPicker/PhotoPicker/AlbumViewController.swift
@@ -7,21 +7,48 @@
 //
 
 import UIKit
+import Photos
 
 extension PhotoPicker {
 
     class AlbumViewController: UITableViewController {
 
+        var collections: [PHAssetCollection] = [PHAssetCollection]() {
+            didSet {
+                tableView.reloadData()
+            }
+        }
+
         override func viewDidLoad() {
             super.viewDidLoad()
 
-            let leftBarButtonItem = UIBarButtonItem(title: "Close", style: .Plain, target: self, action: "dismiss")
-            navigationItem.leftBarButtonItem = leftBarButtonItem
+            configure()
         }
     }
 }
 
 extension PhotoPicker.AlbumViewController {
+
+    func configure() {
+        let leftBarButtonItem = UIBarButtonItem(title: "Close", style: .Plain, target: self, action: "dismiss")
+        navigationItem.leftBarButtonItem = leftBarButtonItem
+
+        tableView.registerClass(UITableViewCell.self, forCellReuseIdentifier: "cell")
+    }
+
+    func getCollectionList() {
+        let result = PHCollectionList.fetchTopLevelUserCollectionsWithOptions(nil)
+
+        var collections = [PHAssetCollection]()
+        result.enumerateObjectsUsingBlock { obj, index, stop in
+            if let collection = obj as? PHAssetCollection {
+                collections.append(collection)
+            }
+            if stop.memory {
+                self.collections = collections
+            }
+        }
+    }
 
     func dismiss() {
         self.dismissViewControllerAnimated(true, completion: {})
