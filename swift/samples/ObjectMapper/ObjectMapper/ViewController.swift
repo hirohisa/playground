db979560383e91e2b6eb7fb282eae96f2da71696
Hirohisa Kawasaki <hirohisa.kawasaki@gmail.com>
2015-04-27 21:13:38 +0900

    Add ObjectMapper

---
+++ swift/samples/ObjectMapper/ObjectMapper/ViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ViewController.swift
+//  ObjectMapper
+//
+//  Created by Hirohisa Kawasaki on 4/27/15.
+//  Copyright (c) 2015 Hirohisa Kawasaki. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+
+
+}
+
