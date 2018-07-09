2e3055f2b9ad3b333080df763d76bc87166d4291
Hirohisa Kawasaki <hirohisa.kawasaki@gmail.com>
2018-07-10 00:16:02 +0900

    Add PullToRefreshViewController

---
+++ swift4/RefreshControlSample/RefreshControlSample/PullToRefreshViewController.swift
@@ -0,0 +1,57 @@
+//
+//  PullToRefreshViewController.swift
+//  RefreshControlSample
+//
+//  Created by Hirohisa Kawasaki on 7/9/18.
+//  Copyright © 2018 Hirohisa Kawasaki. All rights reserved.
+//
+
+import UIKit
+
+class RefreshView: UIView, RefreshHeaderView {
+
+    let indicator = UIActivityIndicatorView(frame: CGRect(x: 0, y: 0, width: 20, height: 20))
+
+    override func didMoveToSuperview() {
+        super.didMoveToSuperview()
+        addSubview(indicator)
+    }
+
+    override func layoutSubviews() {
+        super.layoutSubviews()
+
+        indicator.center = CGPoint(x: frame.width / 2, y: frame.height / 2)
+    }
+
+    func load(state: RefreshState) {
+        switch state {
+        case .default, .isLoadingMore:
+            indicator.stopAnimating()
+        case .isRefreshing:
+            indicator.startAnimating()
+        default:
+            break
+        }
+    }
+}
+
+class PullToRefreshViewController: UIViewController, UIScrollViewDelegate {
+
+    func scrollViewDidEndDragging(_ scrollView: UIScrollView, willDecelerate decelerate: Bool) {
+        if let scrollView = scrollView as? Refreshable {
+            scrollView.observeDragging()
+        }
+    }
+
+    func scrollViewDidScroll(_ scrollView: UIScrollView) {
+        if let scrollView = scrollView as? Refreshable {
+            scrollView.observeScrolling()
+        }
+    }
+
+    func scrollViewWillBeginDragging(_ scrollView: UIScrollView) {
+        if let scrollView = scrollView as? Refreshable {
+            scrollView.observeDragging()
+        }
+    }
+}
